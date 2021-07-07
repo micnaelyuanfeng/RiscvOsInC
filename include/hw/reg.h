cf016819aa1c26f2e82ea1c3cd096cdac788a611
yuan feng <michaelyuan.feng@hotmail.com>
2021-07-06 22:44:53 -0400

    init files

---
+++ include/hw/reg.h
@@ -0,0 +1,8 @@
+#ifndef _REG_H_
+#define _REG_H_
+
+void register_read();
+void register_write();
+void write_stap();
+
+#endif
--- include/trap/trap.h
+++ include/trap/trap.h
@@ -1,3 +1,10 @@
+#ifndef _TRAP_H_
+#define _TRAP_H_
+
 typedef struct Trapframe {
     
 }_trapframe_t;
+
+void dispatch_interrupt();
+
+#endif
