d2f3ed5a469d165eb17737c9432042fc36d1327b
yuan feng <michaelyuan.feng@mail.utoronto.ca>
2021-07-08 19:33:21 -0400

    init files

--- include/trap/interrupt.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INTERRUPT_H_
-#define _INTERRUPT_H_
-
-enum INT_VECT_TBL {
-    
-};
-
-#endif
--- include/trap/trap.h
+++ include/trap/trap.h
@@ -1,12 +1,16 @@
 #ifndef _TRAP_H_
 #define _TRAP_H_
 
+enum INT_VECT_TBL {
+    
+};
+
 typedef struct Trapframe {
     
 }_trapframe_t;
 
 void dispatch_interrupt();
-void dispatch_soft();
-void dispatch_ext();
+void dispatch_soft_interrupt();
+void dispatch_ext_interrupt();
 
 #endif
