a70c43b99c2a6826219ee04d137329f2d455570e
yuan <michaelyuan.feng@mail.utoronto.ca>
2021-08-11 00:28:46 -0400

    build page table map function

--- src/mm/vmm.c
+++ src/mm/vmm.c
@@ -36,7 +36,7 @@ void _buildPageTable(){
 
     VmControl.ptVa = ptVa;
 
-    printf("Allocte 0x%x%x\n", ptVa >> 31 & 0xFFFFFFFF, ptVa & 0xFFFFFFFF);
+    // printf("Allocte 0x%x%x\n", ptVa >> 31 & 0xFFFFFFFF, ptVa & 0xFFFFFFFF);
 
     _updatePageTable(VmControl.ptVa, VmControl.ptPa, PT_LEVEL - 1);
 }
@@ -55,21 +55,66 @@ void _updatePageTable(uint64_t _ptVa, uint64_t _ptPa, uint8_t level){
     };
 
     uint64_t* root = (uint64_t*)VmControl.ptVa;
+    uint64_t pa = 0UL;
+    uint64_t* nextLevelPtVaEntryVa = (uint64_t*)&root[vpn[2]];
 
-    printf("%x\n", *root);
+    printf("%d\n",vpn[2]);
+    printf("%d\n",vpn[1]);
+    printf("%d\n",vpn[0]);
 
-    for (int i = level; i >= 0; i--){
-        if(!root[vpn[i]] & Valid){
-            printf("Not Allocated\n");
-        }else{
-            printf("Allocated\n");
+    for (int i = level - 1; i >= 0; i--){
+        if(!(*nextLevelPtVaEntryVa) & Valid){
+            CoreMemBlkInfo_t blkInf = {0};
+            blkInf.numOfPage = 1;
+            
+            pa = CoreMapControl.kmalloc(&blkInf);
+
+            *nextLevelPtVaEntryVa = (pa >> 2) | Valid;
         }
-        // printf("%x\n", vpn[i]);
+
+        uint64_t* nextLevelPtVa = (uint64_t*)(pa | 0xFFFFFFFF00000000);
+
+        nextLevelPtVaEntryVa = &nextLevelPtVa[vpn[i]];
     }
 
+    uint64_t pageTableEntryVal = (ppn[2] << 28) |   // PPN[2] = [53:28]
+        (ppn[1] << 19) |   // PPN[1] = [27:19]
+        (ppn[0] << 10) |   // PPN[0] = [18:10]
+        Valid | ReadWriteExecute;
+
+    *nextLevelPtVaEntryVa = pageTableEntryVal;
+    
+    for(int i = 0; i <= 511; i++){
+        if(root[i] & Valid){
+            printf("====> PD0 --> Entry value is %d 0x%x%x\n", i, root[i] >> 32, root[i]);
+            uint64_t* ppt = (uint64_t*)(((root[i]) >> 1 << 3) | 0xFFFFFFFF00000000);
+            
+            for(int j = 0; j <= 511; j++){
+                if(ppt[j] & Valid){
+                    printf("====>   PD1 --> Entry value is %d 0x%x%x\n", j, ppt[j] >> 32, ppt[j]);
+
+                    uint64_t* ppe = (uint64_t*)(((ppt[j]) >> 1 << 3) | 0xFFFFFFFF00000000);
+
+                    for(int k = 0; k <= 511; k++){
+                        if(ppe[k] & Valid){
+                            printf("====>      PE ---> Entry value is %d 0x%x%x\n", k, ppe[k] >> 32, ppe[k]);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
 
 void _registerPageTable(){
+    
+}
+
+void fnPtWalk(){
+
+}
+
+void fnMapPaToVaTest(){
     
 }
