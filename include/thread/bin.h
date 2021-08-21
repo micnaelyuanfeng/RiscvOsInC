77a86b08f8fa1d16b90e25eb1734940062fd0ed0
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2021-08-21 12:31:05 -0400

    add bin header

---
+++ include/thread/bin.h
@@ -0,0 +1,22 @@
+#ifndef _BIN_H_
+#define _BIN_H_
+
+#define BIN_START_VA 0xf80200000
+#define BIN_SIZE 0x30
+
+unsigned int binArray2[] = {
+   0xfe010113,
+   0x00813c23,
+   0x02010413,          	
+   0x802637b7,          	
+   0x03078793,          	
+   0xfef43423,          	
+   0xfe843783,          	
+   0x37ab7737,          	
+   0x00271713,          	
+   0xeff70713,          	
+   0x00e7b023,
+   0xfedff06f, 
+};
+
+#endif 
