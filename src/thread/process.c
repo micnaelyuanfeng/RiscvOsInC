7551fa6a9c75d4abcb613fdd39486b7381ca48b5
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2021-08-23 22:15:35 -0400

    add process file

---
+++ src/thread/process.c
@@ -0,0 +1,29 @@
+#include "types.h"
+#include "process.h"
+
+void fnMapGlobalMemInit(){
+
+}
+
+void fnMapGlobalMemWrite(){
+
+}
+
+void fnMapGlobalMemRead(){
+
+}
+
+
+void fnLockMemoryWrite(){
+
+}
+
+void fnLockMemoryRead(){
+
+}
+
+void fnEntry(){
+    while(1){
+        
+    }
+}
