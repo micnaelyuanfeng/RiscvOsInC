800dd714c6ae74f13ee7ca0ad904aeff2599085e
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2021-09-05 15:45:20 -0400

    lock update

--- include/lock.h
+++ include/lock.h
@@ -3,9 +3,14 @@
 
 void __getLock();
 void __releaseLock();
+void __getCbLock();
+void __releaseCbLock();
 
 #define acquireLock(...) __getLock()
 #define releaseLock(...) __releaseLock()
+#define acquireCbLock(...) __getCbLock()
+#define releaseCbLock(...) __releaseCbLock()
+
 #define wfi(...) ({ \
     __asm__ volatile ("wfi");   \
 })
